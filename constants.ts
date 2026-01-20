c3dc62041a8c5f6c3d8d870bfd909fac1d0ad640
cfpgamelive <sumitraaryal080@gmail.com>
2026-01-20 01:43:21 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const AFFILIATE_LINK = "https://trusthubmedia.com/app/aff/go/unzmnx?i=74&keyword=nanaxnana";
+export const AFFILIATE_LINK = "https://trusthubmedia.com/app/aff/go/unzmnx?i=7&keyword=nanaxnana";
 export const BANNER_IMAGE_URL = "https://a57.foxsports.com/statics.foxsports.com/www.foxsports.com/content/uploads/2026/01/1294/728/cfp_cfb_title-game_2026.jpg?ve=1&tl=1";
 
 export const APP_NAME = "CFP Live Pass";
